9ac1df59371e743988860460f38540a68c06ddbf
CaptMD-11 <86084719+CaptMD-11@users.noreply.github.com>
2023-01-03 20:53:51 -0800

    added license to source code

--- src/VCSV.java
+++ src/VCSV.java
@@ -1,3 +1,21 @@
+/*
+ * This is VCSV, a Java library made by Vignesh Nydhruva, that helps run VStats functions on CSV files. 
+    Copyright (C) 2022  Vignesh Nydhruva
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
